9470e164f5b0445b8ffbe12c98597f683cdca87b
David Eliason <davethemaker@gmail.com>
2020-02-17 15:53:22 -0800

    update spider to return each and every ad

--- craigslist_web_scraping/spiders/rentals.py
+++ craigslist_web_scraping/spiders/rentals.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
+# \1. Title
+# 2. Posted Date
+# 3. Rental Price
+# 4. Number of bedrooms
+# 5. Neighborhood
+# 6. Ad Text Content (description)
+
 import scrapy
 
 
@@ -8,8 +15,14 @@ class RentalsSpider(scrapy.Spider):
     start_urls = ['https://seattle.craigslist.org/d/vacation%E2%80%90rentals/search/vac/']
 
     def parse(self, response):
-        titles = response.xpath('//a[@class="result-title hdrlnk"]/text()').extract()
+        rentals = response.xpath('//p[@class="result-info"]')
+        # titles = response.xpath('//a[@class="result-title hdrlnk"]/text()').extract()
+        # posted_dates = response.xpath('//time[@class="result-date"]/text()').extract()
         
-        for title in titles:
-            yield {'Title': title}
+       # for title in titles:
+        #    yield {'Title': title}
+            
+       # for date in posted_dates:
+       #     yield {'Time': date}
+        print(rentals)
  
