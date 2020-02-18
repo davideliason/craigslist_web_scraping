968a82de153c72f61629a5d681fda6bdae8311ee
David Eliason <davethemaker@gmail.com>
2020-02-17 16:54:43 -0800

    scrape neighborhood

--- craigslist_web_scraping/spiders/rentals.py
+++ craigslist_web_scraping/spiders/rentals.py
@@ -26,10 +26,15 @@ class RentalsSpider(scrapy.Spider):
             title = rental.xpath('a/text()').extract_first()
             result_date = rental.xpath('time[@class="result-date"]/text()').extract()
             price = rental.xpath('span[@class="result-meta"]/span[@class="result-price"]/text()').extract()
+            rooms = rental.xpath('span[@class="result-meta"]/span[@class="housing"]/text()').extract()
+            neighborhood = rental.xpath('span[@class="result-meta"]/span[@class="result-hood"]/text()').extract_first("")[2:-1]
+            
             
             yield{'Title': title,
                  'Date':result_date,
-                 'Price': price}
+                 'Price': price,
+                 'Rooms': rooms,
+                 'Neighborhood' : neighborhood}
             
  
       
